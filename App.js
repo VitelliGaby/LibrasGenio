65cd341eda7f8312138a2803d1f3a3583dfc64f6
antonio-qsneto <prof.antonioqsneto@gmail.com>
2024-09-01 22:11:03 -0300

    comentarios em App.js

--- App.js
+++ App.js
@@ -11,14 +11,18 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { View, Text, Pressable, TouchableOpacity } from "react-native";
 
 const App = () => {
+  
+  // Estado para controlar a exibição da tela de splash (carregamento)
   const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
 
+  // Hook para carregar fontes personalizadas
   const [fontsLoaded, error] = useFonts({
     "Jost-SemiBold": require("./assets/fonts/Jost-SemiBold.ttf"),
     "Mulish-Bold": require("./assets/fonts/Mulish-Bold.ttf"),
     "Montserrat-SemiBold": require("./assets/fonts/Montserrat-SemiBold.ttf"),
   });
 
+  // Se as fontes não foram carregadas e não há erro, retorna null (não renderiza nada)
   if (!fontsLoaded && !error) {
     return null;
   }
@@ -26,23 +30,33 @@ const App = () => {
   return (
     <>
       <NavigationContainer>
+        
+        {/* Condicional para exibir o Stack Navigator se a tela de splash não estiver oculta */}
         {hideSplashScreen ? (
           <Stack.Navigator screenOptions={{ headerShown: false }}>
+            
+            {/* Tela inicial do aplicativo */}
             <Stack.Screen
               name="PaginaInicial"
               component={PaginaInicial}
               options={{ headerShown: false }}
             />
+            
+            {/* Tela de cursos */}
             <Stack.Screen
               name="Cursos"
               component={Cursos}
               options={{ headerShown: false }}
             />
+            
+            {/* Tela sobre nós */}
             <Stack.Screen
               name="SobreNos"
               component={SobreNos}
               options={{ headerShown: false }}
             />
+            
+            {/* Tela de programação */}
             <Stack.Screen
               name="Programacao"
               component={Programacao}
@@ -54,4 +68,5 @@ const App = () => {
     </>
   );
 };
+
 export default App;
